c07f837cb363434ee5931b4bdcfc4d840e602978
Cameron Cardinal <cameriancardinal@gmail.com>
2024-11-26 23:46:42 +0100

    i dunno

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -11,17 +11,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/messages')]
+#[Route('/')]
 final class MessagesController extends AbstractController
 {
-    #[Route(name: 'app_messages_index', methods: ['GET'])]
-    public function index(MessagesRepository $messagesRepository): Response
-    {
-        return $this->render('messages/index.html.twig', [
-            'messages' => $messagesRepository->findAll(),
-        ]);
-    }
-
     #[Route('/new', name: 'app_messages_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -33,7 +25,7 @@ final class MessagesController extends AbstractController
             $entityManager->persist($message);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_messages_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('messages/new.html.twig', [
@@ -59,7 +51,7 @@ final class MessagesController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_messages_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('messages/edit.html.twig', [
@@ -76,6 +68,6 @@ final class MessagesController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_messages_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
     }
 }
